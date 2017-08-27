d2d6ede2fc8ad99293f72e72dc12ca970875bab1
Philipp Allmer <allmer@allmerit.at>
2017-08-27 11:12:39 +0200

    not showing browser by default

--- BrowserActions/Options.cs
+++ BrowserActions/Options.cs
@@ -7,7 +7,7 @@ namespace CefBrowserControl
         public const int LockTimeOut = 100;
         public const bool IsDebug = false;
         public const int MaxBrowserInstances = 3;
-        public const bool WindowsNormallyVisible = true;
+        public const bool WindowsNormallyVisible = false;
         public static TimeSpan DefaultCefBrowserActionOrCommandTimeoutMsec = new TimeSpan(0, 0, 0,30);
         public const string DefaultUrl = "https://duckduckgo.com";
         //Keepass DB X Password Changer Exported Template
