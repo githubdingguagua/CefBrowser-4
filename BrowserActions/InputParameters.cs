decd296a8ee749f6f252ee56d3fb0ddee3e936da
Philipp Allmer <allmer@allmerit.at>
2017-09-01 05:05:08 +0200

    ignoring properties which have not to be serialized

--- BrowserActions/InputParameters.cs
+++ BrowserActions/InputParameters.cs
@@ -2,15 +2,18 @@
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace CefBrowserControl
 {
     public abstract class InputParameters
     {
+        [XmlIgnore]
         public bool HaveRequirementsBeenSet;
 
         public List<KeyValuePairEx<string, object>> InputParameterAvailable;
 
+        [XmlIgnore]
         public List<KeyValuePairEx<string, object>> InputParameterSet;
 
         public static List<string> InputParameterRequired;
